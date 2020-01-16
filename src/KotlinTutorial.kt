17ddb8df4b7f01beee683aaccda7fba577470d18
MRomaneck <mark.romaneck@gmail.com>
2020-01-15 19:38:31 -0600

    first kommit

---
+++ src/KotlinTutorial.kt
@@ -0,0 +1,33 @@
+fun main ()
+
+{
+
+    var i = 100
+    var divisible = null
+    val j = divisible
+
+    var seven = 7
+
+
+    for (i:Int in 0..100 step 7) println ("wuirto agregar ${i}")
+
+}
+    //for(i:Int in 1..100 step 7) print("divisible by 7")}
+
+/*
+        (n (10*j)
+        seven--
+
+            if (i >= 7) 
+
+            println("divided by 7")
+
+    while (i>=1)
+
+    println("Divisible by 7 - ${i / seven}"))
+}
+/*
+
+
+
+ */
