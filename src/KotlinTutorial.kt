2fc541fd8cdc2008d1caf85b0d81526e10af2c5f
MRomaneck <mark.romaneck@gmail.com>
2020-01-24 22:05:34 -0600

    Get max

--- src/KotlinTutorial.kt
+++ src/KotlinTutorial.kt
@@ -1,34 +1,14 @@
-fun main ()
-
-{
-
-    var i = 100
-    var divisible = null
-    val j = divisible
-
-    var seven = 7
-
-
-
-    for (i:Int in 0..100 step 7) println ("wuirto agregar ${i}")
-
+fun main () {
+    println(getMax(a = 11, b = 5, c = 7))
 }
-    //for(i:Int in 1..100 step 7) print("divisible by 7")}
 
-/*
-        (n (10*j)
-        seven--
+fun getMax(a:Int, b:Int) = if (a > b) a else b
 
-            if (i >= 7) 
+fun getMax(a:Double, b:Double) = if (a > b) a else b
 
-            println("divided by 7")
-
-    while (i>=1)
-
-    println("Divisible by 7 - ${i / seven}"))
-}
-/*
-
-
-
- */
+fun getMax(a: Int, b: Int, c: Int) =
+    when {
+        a >= b && a >=c -> a
+        b >= a && b >=c -> b
+        else -> c
+    }
